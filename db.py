95f875338deda266342cc215c17ad67f4cb8c8cd
Muhammaddiyor09 <malisherov92@gmail.com>
2025-08-11 14:22:37 +0500

    db code replace mysql to sqlite

--- db.py
+++ db.py
@@ -1,13 +1,28 @@
+# from sqlalchemy import create_engine
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import sessionmaker
+#
+# engine = create_engine('mysql+pymysql://root@localhost:3306/lesson')
+# SessionLocal = sessionmaker(bind=engine)
+#
+#
+# Base = declarative_base()
+#
+#
+# def database():
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('mysql+pymysql://root@localhost:3306/lesson')
+engine = create_engine("sqlite:///./database.db", connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine)
-
-
 Base = declarative_base()
-
 
 def database():
     db = SessionLocal()
@@ -15,3 +30,25 @@ def database():
         yield db
     finally:
         db.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
